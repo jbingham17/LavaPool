3d3034ff324c84d05e45cd23d523e8ff344dca12
John Bingham <johnbingham@chobingsmacbook.lan>
2022-05-18 00:37:12 -0400

    Fixed APR

--- client/js/callAPI.js
+++ client/js/callAPI.js
@@ -1,27 +1,34 @@
 //Set fake values for the APYs without the API call
 $( document ).ready(function() {
-  $("#rocketpool-apy").text("4.06");
-  $("#lido-apy").text("3.77");
-  $("#stakewise-apy").text("4.0");
-  $("#ankr-apy").text("5.03");
-  let ourAPY = (4.06 + 3.77 + 4.0 + 5.03 )/4;
-  console.log(ourAPY)
-  $("#our-apy").text(ourAPY);
+  // $("#rocketpool-apy").text("4.06");
+  // $("#lido-apy").text("3.77");
+  // $("#stakewise-apy").text("4.0");
+  // $("#ankr-apy").text("5.03");
+  // let ourAPY = (4.06 + 3.77 + 4.0 + 5.03 )/4;
+  // console.log(ourAPY)
+  // $("#our-apy").text(String(ourAPY) + "%");
 });
 
 //jQuery API call to get yield values
-$.get( "https://selenium-scrape-results2.s3.amazonaws.com/outputs/output.json", function( data ) {
-    console.log(data)
-     $("#rocketpool-apy").text(data.rocketpool);
-     $("#lido-apy").text(data.lido);
-     $("#stakewise-apy").text(data.stakewise);
-     $("#ankr-apy").text(data.ankr);
+$.get( "https://selenium-scrape-results2.s3.amazonaws.com/outputs/output.json", function( data, status ) {
+    console.log(String(data));
+    const obj = JSON.parse(data);
 
-     let ourAPY = (4.06 + 3.77 + 4.0 + 5.03 )/4;
-     console.log(ourAPY)
-     $("#our-apy").text(ourAPY);
+    const rpl = parseFloat(obj["RocketPool"]);
+    const lido = parseFloat(obj["Lido"]);
+    const swise = parseFloat(obj["Stakewise"]);
+    const ankr = parseFloat(obj["Ankr"]);
 
-    alert( "Load was performed." );
+     $("#rocketpool-apy").text(String(rpl) + "%");
+     $("#lido-apy").text(String(lido) + "%");
+     $("#stakewise-apy").text(String(swise) + "%");
+     $("#ankr-apy").text(String(ankr) + "%");
+
+     const ourAPY = ((rpl + lido + swise + ankr)/4).toFixed(2);
+     console.log(ourAPY);
+     $("#our-apy").text(String(ourAPY) + "%");
+
+    // alert( "Load was performed." + status);
   });
 
 
